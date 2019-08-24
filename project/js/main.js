9125add6110dc9f30e3ff9077abdb4d7cf4165bb
Brandon Kirschner <bkirschner@hubspot.com>
2019-08-24 15:58:53 -0400

    Completed Course

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,79 @@
+$(document).ready(function() {
+  getPosts();
+});
+
+function handleSignIn() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+      console.log(user.email);
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+}
+
+function addMessage(postTitle, postBody) {
+  var postData = {
+    title: postTitle,
+    body: postBody
+  };
+
+  var database = firebase.database().ref("posts");
+
+  // Create a new post reference with an auto-generated id
+  var newPostRef = database.push();
+  newPostRef.set(postData, function(error) {
+    if (error) {
+    } else {
+      window.location.reload();
+    }
+  });
+}
+
+function handleMessageFormSubmit() {
+  var postTitle = $("#post-title").val();
+  var postBody = $("#post-body").val();
+  addMessage(postTitle, postBody);
+}
+
+function getPosts() {
+  return firebase
+    .database()
+    .ref("posts")
+    .once("value")
+    .then(function(snapshot) {
+      var posts = snapshot.val();
+      for (var postKey in posts) {
+        var post = posts[postKey];
+        console.log(post);
+        $("#post-listing").append(
+          "<div>" + post.title + " - " + post.body + "</div>"
+        );
+      }
+    });
+}
+
 function getWeather(searchQuery) {
   var url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     searchQuery +
     "&units=imperial&APPID=" +
-    apiKey;
+    weatherApiKey;
 
   $(".city").text("");
   $(".temp").text("");
